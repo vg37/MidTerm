c9299833ff821fda745e84093c39b2668b17adde
venu <vg37@zips.uakron.edu>
2015-11-04 19:00:27 -0500

    Initial Version

---
+++ MidCompare/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  MidCompare
+//
+//  Created by Gangisetty,Venugopal on 11/4/15.
+//  Copyright © 2015 Gangisetty,Venugopal. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
